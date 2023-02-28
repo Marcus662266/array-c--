423dcd89eb7e5867ddd714982c7370fc047c649d
Marcus662266 <lanciotti.marco97@gmail.com>
2023-02-28 12:20:18 +0100

    array4

---
+++ array3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <array>
+#include <string>
+#include <cmath>
+using namespace std;
+int main ()
+{
+    int arrayInteri[10]={0,1,2,3,4,5,6,7,8,9}; //dichiaro array
+    int arraylenght= sizeof(arrayInteri)/sizeof(arrayInteri[0]); // dichiaro come calcolare lunghezza array
+    int somm= 0;
+    for(int i=0;)
+
+}
