2cf387826c505b9159733214514155370e542429
Marcus662266 <lanciotti.marco97@gmail.com>
2023-03-02 22:09:13 +0100

    array8 sicuro un problema di scrittura

--- array7.cpp
+++ array7.cpp
@@ -1,11 +1,9 @@
-/*Scrivi una funzione che prenda in input un array di interi e 
-la sua lunghezza e restituisca il numero di sequenze di 
-interi crescenti all'interno dell'array. Ad esempio, se 
-l'array di input è {1, 2, 3, 5, 4, 7}, la funzione dovrebbe 
-restituire 2 perché ci sono due sequenze crescenti: {1, 2, 
+/*Scrivi una funzione che prenda in input un array di interi e
+la sua lunghezza e restituisca il numero di sequenze di
+interi crescenti all'interno dell'array. Ad esempio, se
+l'array di input è {1, 2, 3, 5, 4, 7}, la funzione dovrebbe
+restituire 2 perché ci sono due sequenze crescenti: {1, 2,
 3} e {4, 7}. */
-
-
 
 #include <iostream>
 #include <array>
@@ -35,9 +33,11 @@ int arrayinput(int array[], int arraylengh)
 
 int main()
 {
-    int array[] = {1, 2, 3, 5, 4, 7};                    // dichiarop array
-    int arraylenght = sizeof(array) / sizeof(array[0]);  // dichiaro lunghezza array
-    int numeroSequenza = arrayinput(array, arraylenght); // imposto operazione da richiamare
-    cout << "numersequenze  " << numeroSequenza << endl; // stampami il numero delle sequenze
+    int array[]={1,2,3,4,1,2,3,4,1,2,3,4};     // dichiaro array
+    
+        
+        int arraylenght = sizeof(array) / sizeof(array[0]); // dichiaro lunghezza array
+    int numeroSequenza = arrayinput(array, arraylenght);    // imposto operazione da richiamare
+    cout << "numersequenze  " << numeroSequenza << endl;    // stampami il numero delle sequenze
     return 0;
 }
--- array8.cpp
+++ array8.cpp
@@ -3,5 +3,32 @@
 #include <string>
 #include <cmath>
 using namespace std;
+int calcolamax(int arrayInteri[], int arrayInterilen)
+{ // trova il numero più grande dell'array
+    int max;
+    int calcolamax = 0;
+    // guarda tra tutti gli elementi dell' arrayInteri
+    for (int i = 0; i < arrayInterilen; i++)
+    {
+        cout << "";
+        i++;
 
+        // se numero preso è maggiore del precedente
+        if (arrayInteri[i] > calcolamax)
+        {
+            // assegna il nuovo massimo
+            arrayInteri[i] = max;
+        }
+    }
+    return max;
+}
 int main()
+{
+    // dichiaro larray
+    int arrayInteri[] = {2, 1, 2, 3};
+    // dichiaro la lungezza dell array
+    int arrayInterilen = sizeof(arrayInteri) / sizeof(arrayInteri[0]);
+    // dichiaro max,apro la funzione che richiamo fuori dal main
+    int max = calcolamax(arrayInteri, arrayInterilen);
+    cout << "stampa il numero piu alto  " << max << endl;
+}
