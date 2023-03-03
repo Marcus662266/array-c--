043ca48ed69f4902b7dfc88f8dd7f3479f7e6771
Marcus662266 <lanciotti.marco97@gmail.com>
2023-03-03 17:22:04 +0100

    array9 problema di sintassi o programma?

--- array9.cpp
+++ array9.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 
 int arrayInteri1[],  arrayInteri1len, arrayInteri2[], arrayInteri2len ;
-int comune(int arrayInteri1[], int arrayInteri1len, arrayInteri2[], arrayInteri2len)
+int comune(int arrayInteri1[], int arrayInteri1len, arrayInteri2[], arrayInteri2len );
 {
     int contatore = 0;
     for (i = 0; i < arrayInteri1len; i++)
@@ -24,12 +24,11 @@ int main()
 {
 
     int arrayInteri1[] = {2, 1, 9, 8}; // dichiaro larray 1
-    int arrayInteri2[] = {1, 2, 7, 5}                                            // dichiaro array 2
+    int arrayInteri2[] = {1, 2, 7, 5}; // dichiaro array 2
     int arrayInteri1len = sizeof(arrayInteri1) / sizeof(arrayInteri1[0]); // dichiaro la lungezza dell array 1
     int arrayInteri2len = sizeof(arrayInteri2) / sizeof(arrayInteri2[0]); // lunghezza array 2
-    int quanti = comune(arrayInteri1, arrayInteri1len, arrayInteri2, arrayInteri2len)
-                     cout
-                 << "stampa quanti sono i numeri in comune  " << quanti << endl;
+    int quanti = comune(arrayInteri1, arrayInteri1len, arrayInteri2, arrayInteri2len);
+  cout << "stampa quanti sono i numeri in comune  " << quanti << endl;
 
     return 0;
 }
