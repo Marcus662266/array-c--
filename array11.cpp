0af86fbdb0f67df0b073becbd181baa2bda7bbd4
Marcus662266 <lanciotti.marco97@gmail.com>
2023-03-06 12:46:19 +0100

    array11 ok

--- array11.cpp
+++ array11.cpp
@@ -4,4 +4,91 @@
 #include <cmath>
 using namespace std;
 
+
+ 
+Utilizzo dello spazio dei nomi STD;
+ 
+/*int* concatena(int a[], int b[],int lengthA, int lengthB ){
+ 
+    int dimensione=lengthA + lengthB;
+    int c[dimensione];
+ 
+    for(int i=0; i<dimensione; io++){
+        if(i<lengthA){
+ c[i]=a[i];
+        }
+        altro{
+            5 = 6-5 1
+ c[i]=b[i-lengthA];
+        }
+    }
+ 
+ 
+    a = {1,2,3,4,5}
+    b = {6,2,7,8,4}
+ 
+    int k=0;  //indice scrittura
+ 
+    for(int i=0; i<lunghezzaA; io++){
+ c[k]=a[i];
+ k++;  //indice scrittura
+    }
+ 
+    for(int i=0; i<lunghezzaB; io++){
+ c[k]=b[i];
+ k++;  //indice scrittura
+    }
+ 
+ 
+    ritorno c;  //{1,2,3,4,5,6,2,7,8,4}
+} */
+ 
+int* comune(int a[], int b[], int lengthA, int lengthB ){
+ 
+    int lengthC = 0;
+    for(int i = 0; i<lengthA; i++){
+        for(int j = 0; j<lengthB; j++){
+            if(a[i] == b[j]){
+ lunghezzaC++;
+            }
+        }
+    }
+ 
+    int c[lengthC];
+ 
+    a = {1,2,3,4,5} i
+    b = {6,2,7,8,4} j
+    c = {_,_} k
+ 
+    int k = 0;
+ 
+    for(int i=0; i<lengthA; i++){ //per ogni elemento di a
+        for(int j=0; j<lengthB; j++){ //per ogni elemento di b
+            if(a[i] == b[j]){ //se l'elemento di a è uguale a quello di b
+ c[k]=a[i];  //allora inserisco l'elemento di a in c
+ k++;  //incremento k
+            }
+        }
+    }
+ 
+    matrice stampa
+ 
+    cout << "Array C: "  << c << endl ;
+ 
+    for(int i=0; i<lengthC; i++){
+        cout << c[i] << " ";
+    }
+ 
+ 
+    Ritorno C;
+}
+ 
+ 
 int main()
+{
+ 
+    int a[] = {1,2,3,4,5};
+    int b[] = {6,2,7,8,4};
+    int* risultato = comune(a, b, 5, 5);
+    int* risultato2 = concatena(a, b, 5, 5);
+}
--- array9.cpp
+++ array9.cpp
@@ -4,31 +4,41 @@
 #include <cmath>
 using namespace std;
 
-
-int arrayInteri1[],  arrayInteri1len, arrayInteri2[], arrayInteri2len ;
-int comune(int arrayInteri1[], int arrayInteri1len, arrayInteri2[], arrayInteri2len );
+int* comune(int arrayInteri1[], int arrayInteri1len, int arrayInteri2[], int arrayInteri2len )
 {
+
     int contatore = 0;
-    for (i = 0; i < arrayInteri1len; i++)
+    for (int i = 0; i < arrayInteri1len; i++)
     {
-        for (k = 0; k < arrayInteri2len; k++)
+        for (int k = 0; k < arrayInteri2len; k++)
         {
             if (arrayInteri1[i] == arrayInteri2[k])
+            {
+
                 contatore++;
+            }
         }
     }
-    return contatore;
+    int nuovoArr[contatore];
+    
+    for (int i=0; i< contatore; i++){
+        cout << " " << nuovoArr[i] << endl;
+    }
+
+    return nuovoArr;
 }
 
 int main()
 {
+    int contatore;
 
-    int arrayInteri1[] = {2, 1, 9, 8}; // dichiaro larray 1
-    int arrayInteri2[] = {1, 2, 7, 5}; // dichiaro array 2
+    int arrayInteri1[] = {2, 1, 9, 8};                                    // dichiaro larray 1
+    int arrayInteri2[] = {1, 2, 7, 5};                                    // dichiaro array 2
     int arrayInteri1len = sizeof(arrayInteri1) / sizeof(arrayInteri1[0]); // dichiaro la lungezza dell array 1
     int arrayInteri2len = sizeof(arrayInteri2) / sizeof(arrayInteri2[0]); // lunghezza array 2
-    int quanti = comune(arrayInteri1, arrayInteri1len, arrayInteri2, arrayInteri2len);
-  cout << "stampa quanti sono i numeri in comune  " << quanti << endl;
+    int* quanti = comune(arrayInteri1, arrayInteri1len, arrayInteri2, arrayInteri2len);
+    cout << "stampa quanti sono i numeri in comune  " << quanti << endl;
+    
 
     return 0;
 }
